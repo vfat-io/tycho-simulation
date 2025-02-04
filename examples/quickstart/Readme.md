781f0171c6ead2cef69e520e57163aa95885110c
Diana Carvalho <diana@propellerheads.xyz>
2025-02-04 18:18:49 +0000

    feat: Refactor quickstart
    Instead of looping through the top 10 pools and calculating spot prices and amount out for each block, it will return the best amount out for a token pair.
    The trade tokens can be defined through CLI parameters
    
    --- don't change below this line ---
    ENG-4088 Took 5 hours 45 minutes
    
    Took 1 hour 38 minutes
    
    Took 18 minutes
    
    
    Took 2 minutes
    
    Took 5 minutes
    
    Took 1 minute

--- examples/quickstart/Readme.md
+++ examples/quickstart/Readme.md
@@ -3,9 +3,7 @@
 This quickstart guide enables you to:
 
 1. Retrieve data from the Tycho Indexer.
-2. Leverage Tycho Simulation to:
-    1. Calculate spot prices.
-    2. Simulate token swaps.
+2. Leverage Tycho Simulation to get the best amount out of a trade.
 
 ## How to run
 
@@ -13,6 +11,14 @@ This quickstart guide enables you to:
 export RPC_URL=<your-eth-rpc-url>
 cargo run --release --example quickstart
 ```
+
+By default, the example will trade 1 WETH -> USDC. If you want a different trade you can do:
+
+```bash
+cargo run --release --example quickstart -- --sell-token "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" --buy-token "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599" --sell-amount 10000
+```
+
+for 10000 USDC -> WBTC.
 
 See [here](https://docs.propellerheads.xyz/tycho/for-solvers/tycho-quickstart) a complete guide on how to run the
 Quickstart example.
--- examples/quickstart/main.rs
+++ examples/quickstart/main.rs
@@ -1,8 +1,14 @@
-use std::{collections::HashMap, env};
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    str::FromStr,
+};
 
+use clap::Parser;
 use futures::StreamExt;
 use num_bigint::BigUint;
 use tracing_subscriber::EnvFilter;
+use tycho_core::Bytes;
 use tycho_simulation::{
     evm::{
         engine_db::tycho_db::PreCachedDB,
@@ -21,6 +27,19 @@ use tycho_simulation::{
     utils::load_all_tokens,
 };
 
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long, default_value = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")]
+    sell_token: String,
+    #[arg(short, long, default_value = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")]
+    buy_token: String,
+    #[arg(short, long, default_value_t = 1)]
+    sell_amount: u32,
+    /// The tvl threshold to filter the graph by
+    #[arg(short, long, default_value_t = 100.0)]
+    tvl_threshold: f64,
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
@@ -33,8 +52,8 @@ async fn main() {
     let tycho_api_key: String =
         env::var("TYCHO_API_KEY").unwrap_or_else(|_| "sampletoken".to_string());
 
-    let tvl_threshold = 10_000.0;
-    let tvl_filter = ComponentFilter::with_tvl_range(tvl_threshold, tvl_threshold);
+    let cli = Cli::parse();
+    let tvl_filter = ComponentFilter::with_tvl_range(cli.tvl_threshold, cli.tvl_threshold);
 
     let all_tokens = load_all_tokens(
         tycho_url.as_str(),
@@ -45,7 +64,27 @@ async fn main() {
         None,
     )
     .await;
+
+    let sell_token_address =
+        Bytes::from_str(&cli.sell_token).expect("Invalid address for sell token");
+    let buy_token_address = Bytes::from_str(&cli.buy_token).expect("Invalid address for buy token");
+    let sell_token = all_tokens
+        .get(&sell_token_address)
+        .expect("Sell token not found")
+        .clone();
+    let buy_token = all_tokens
+        .get(&buy_token_address)
+        .expect("Buy token not found")
+        .clone();
+    let amount_in =
+        BigUint::from(cli.sell_amount) * BigUint::from(10u32).pow(sell_token.decimals as u32);
+
+    println!(
+        "Looking for the best swap for {} {} -> {}",
+        cli.sell_amount, sell_token.symbol, buy_token.symbol
+    );
     let mut pairs: HashMap<String, Vec<Token>> = HashMap::new();
+    let mut amounts_out: HashMap<String, BigUint> = HashMap::new();
 
     let mut protocol_stream = ProtocolStreamBuilder::new(&tycho_url, Chain::Ethereum)
         .exchange::<UniswapV2State>("uniswap_v2", tvl_filter.clone(), None)
@@ -69,11 +108,25 @@ async fn main() {
 
     while let Some(message) = protocol_stream.next().await {
         let message = message.expect("Could not receive message");
-        print_calculations(message, &mut pairs);
+        get_best_swap(
+            message,
+            &mut pairs,
+            amount_in.clone(),
+            sell_token.clone(),
+            buy_token.clone(),
+            &mut amounts_out,
+        );
     }
 }
 
-fn print_calculations(message: BlockUpdate, pairs: &mut HashMap<String, Vec<Token>>) {
+fn get_best_swap(
+    message: BlockUpdate,
+    pairs: &mut HashMap<String, Vec<Token>>,
+    amount_in: BigUint,
+    sell_token: Token,
+    buy_token: Token,
+    amounts_out: &mut HashMap<String, BigUint>,
+) {
     println!("==================== Received block {:?} ====================", message.block_number);
     for (id, comp) in message.new_pairs.iter() {
         pairs
@@ -81,31 +134,44 @@ fn print_calculations(message: BlockUpdate, pairs: &mut HashMap<String, Vec<Toke
             .or_insert_with(|| comp.tokens.clone());
     }
     if message.states.is_empty() {
-        println!("No pools were updated this block");
+        println!("No pools of interest were updated this block. The best swap is the previous one");
         return
     }
-    println!("Using only pools that were updated this block...");
-    for (id, state) in message.states.iter().take(10) {
+    for (id, state) in message.states.iter() {
         if let Some(tokens) = pairs.get(id) {
-            let formatted_token_str = format!("{:}/{:}", &tokens[0].symbol, &tokens[1].symbol);
-            println!("Calculations for pool {:?} with tokens {:?}", id, formatted_token_str);
-            state
-                .spot_price(&tokens[0], &tokens[1])
-                .map(|price| println!("Spot price {:?}: {:?}", formatted_token_str, price))
-                .map_err(|e| eprintln!("Error calculating spot price for Pool {:?}: {:?}", id, e))
-                .ok();
-            let amount_in =
-                BigUint::from(1u32) * BigUint::from(10u32).pow(tokens[0].decimals as u32);
-            state
-                .get_amount_out(amount_in, &tokens[0], &tokens[1])
-                .map(|result| {
-                    println!(
-                        "Amount out for trading 1 {:?} -> {:?}: {:?} (takes {:?} gas)",
-                        &tokens[0].symbol, &tokens[1].symbol, result.amount, result.gas
-                    )
-                })
-                .map_err(|e| eprintln!("Error calculating amount out for Pool {:?}: {:?}", id, e))
-                .ok();
+            if HashSet::from([&sell_token, &buy_token]) == HashSet::from([&tokens[0], &tokens[1]]) {
+                let amount_out = state
+                    .get_amount_out(amount_in.clone(), &sell_token, &buy_token)
+                    .map_err(|e| {
+                        eprintln!("Error calculating amount out for Pool {:?}: {:?}", id, e)
+                    })
+                    .ok();
+                if let Some(amount_out) = amount_out {
+                    amounts_out.insert(id.clone(), amount_out.amount);
+                }
+                // If you would like to save spot prices instead of the amount out, do
+                // let spot_price = state
+                //     .spot_price(&tokens[0], &tokens[1])
+                //     .ok();
+            }
         }
+    }
+    encode_best_amount_out(amounts_out);
+}
+
+fn encode_best_amount_out(amounts_out: &HashMap<String, BigUint>) {
+    if let Some((key, amount_out)) = amounts_out
+        .iter()
+        .max_by_key(|(_, value)| value.to_owned())
+    {
+        println!(
+            "Pool with the highest amount out: {} with {} (out of {} results)",
+            key,
+            amount_out,
+            amounts_out.len()
+        );
+        // TODO: encode using tycho-execution
+    } else {
+        println!("There aren't pools with the tokens we are looking for");
     }
 }
